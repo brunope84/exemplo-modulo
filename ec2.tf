c96c6c82037a4730eac41f26f0b98d8d928e300a
brunope84 <79951687+brunope84@users.noreply.github.com>
2021-12-09 20:33:17 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-04902260ca3d33422"
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
-    tags = {
+
+  tags = {
     Name = "${var.instance_name}"
   }
 }
